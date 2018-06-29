626e9fbe3ec5de6f8f29d29fd977edc1921a3306
Qureshi <Qureshi@192.168.10.4>
2018-06-28 21:59:48 -0700

    change in main class

--- GitDemo/src/com/main/main.java
+++ GitDemo/src/com/main/main.java
@@ -4,7 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a,b;
+		a=10;b=10;
+		int c=a+b;
+		System.out.println("Output : "+c);
 	}
+
 
 }
