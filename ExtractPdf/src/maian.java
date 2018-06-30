5a745981b195b17031c154931ae5597c6712dfd2
mnhaj <40682854+mnhaj@users.noreply.github.com>
2018-06-30 06:42:40 -0700

    Add files via upload

---
+++ ExtractPdf/src/maian.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		 PdfManager pdfManager = new PdfManager();
+	       pdfManager.setFilePath("C://Users//Qureshi//Desktop//Rozee.pk.pdf");
+	       System.out.println(pdfManager.ToText()); 
+	}
+
+}
